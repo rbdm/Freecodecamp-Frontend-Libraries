abb7a6551e3ce141690e2546daba33ad215cabd6
rbdm <mirhadydorodjatun@gmail.com>
2018-09-05 23:36:35 +0300

    solutions to React Chapter up to Challenge #30

--- React.jsx
+++ React.jsx
@@ -518,32 +518,199 @@ class MyComponent extends React.Component {
 };
 
 /* Challenge 26 */
-/*  */
-
-
+/* Use State to Toggle an Element */
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visibility: false
+    };
+    // change code below this line
+    this.toggleVisibility = this.toggleVisibility.bind(this);
+    // change code above this line
+  }
+  // change code below this line
+  toggleVisibility() {
+    this.state.visibility ?
+      this.setState({
+        visibility: false
+      })
+      : this.setState({
+          visibility: true
+        });
+  }
+  // change code above this line
+  render() {
+    if (this.state.visibility) {
+      return (
+        <div>
+          <button onClick={this.toggleVisibility}>Click Me</button>
+          <h1>Now you see me!</h1>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <button onClick={this.toggleVisibility}>Click Me</button>
+        </div>
+      );
+    }
+  }
+};
 
 /* Challenge 27 */
-/*  */
-
-
+/* Write a Simple Counter */
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+    // change code below this line
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
+    // change code above this line
+  }
+  // change code below this line
+  increment() {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+  decrement() {
+    this.setState({
+      count: this.state.count - 1
+    })
+  }
+  reset() {
+    this.setState({
+      count: 0
+    })
+  }
+  // change code above this line
+  render() {
+    return (
+      <div>
+        <button className='inc' onClick={this.increment}>Increment!</button>
+        <button className='dec' onClick={this.decrement}>Decrement!</button>
+        <button className='reset' onClick={this.reset}>Reset</button>
+        <h1>Current Count: {this.state.count}</h1>
+      </div>
+    );
+  }
+};
 
 /* Challenge 28 */
-/*  */
-
+/* Create a Controlled Input */
+class ControlledInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    };
+    // change code below this line
+    this.handleChange = this.handleChange.bind(this);
+    // change code above this line
+  }
+  // change code below this line
+  handleChange(event) {
+    this.setState({
+      input: event.target.value
+    })
+  }
+  // change code above this line
+  render() {
+    return (
+      <div>
+        { /* change code below this line */}
+        <input value={this.state.input} onChange={this.handleChange} />
+        { /* change code above this line */}
+        <h4>Controlled Input:</h4>
+        <p>{this.state.input}</p>
+      </div>
+    );
+  }
+};
 
 
 /* Challenge 29 */
-/*  */
-
+/* Create a Controlled Form */
+class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      submit: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+  handleSubmit(event) {
+    // change code below this line
+    event.preventDefault(); // to prevent refresh & open in new tab
+    this.setState({
+      submit: this.state.input
+    });
+    // change code above this line
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          { /* change code below this line */ }
+          <input value={this.state.input} onChange={this.handleChange} />
+          { /* change code above this line */ }
+          <button type='submit'>Submit!</button>
+        </form>
+        { /* change code below this line */ }
+        <h1>{this.state.submit}</h1>
+        { /* change code above this line */ }
+      </div>
+    );
+  }
+};
 
 
 /* Challenge 30 */
-/*  */
+/* Pass State as Props to Child Components */
+class MyApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'CamperBot'
+    }
+  }
+  render() {
+    return (
+       <div>
+         <Navbar name={this.state.name} />
+       </div>
+    );
+  }
+};
 
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+    <div>
+      <h1>Hello, my name is: {this.props.name} </h1>
+    </div>
+    );
+  }
+};
 
 
 /* Challenge 31 */
-/*  */
+/* Pass a Callback as Props */
 
 
 
