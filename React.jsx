cc27716155d4190e6da0f9cead8e1e00e74312ad
rbdm <mirhadydorodjatun@gmail.com>
2018-09-09 22:54:49 +0700

    solutions to React Chapter up to Challenge #42 & Review previous challenges

--- React.jsx
+++ React.jsx
@@ -1086,12 +1086,80 @@ class MyComponent extends React.Component {
 };
 
 /* Challenge 41 */
-/*  */
-
+/* Use && for a More Concise Conditional */
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: true
+    }
+    this.toggleDisplay = this.toggleDisplay.bind(this);
+  }
+  toggleDisplay() {
+    this.setState({
+      display: !this.state.display
+    });
+  }
+  render() {
+    // change code below this line
+    return (
+       <div>
+         <button onClick={this.toggleDisplay}>Toggle Display</button>
+         {this.state.display && <h1>Displayed!</h1>}
+       </div>
+    );
+  }
+};
 
 /* Challenge 42 */
-/*  */
+/* Use a Ternary Expression for Conditional Rendering */
+const inputStyle = {
+  width: 235,
+  margin: 5
+}
 
+class CheckUserAge extends React.Component {
+  constructor(props) {
+    super(props);
+    // change code below this line
+    this.state = {
+      input: '',
+      userAge: ''
+    }
+    // change code above this line
+    this.submit = this.submit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({
+      input: e.target.value,
+      userAge: ''
+    });
+  }
+  submit() {
+    this.setState({
+      userAge: this.state.input
+    });
+  }
+  render() {
+    const buttonOne = <button onClick={this.submit}>Submit</button>;
+    const buttonTwo = <button>You May Enter</button>;
+    const buttonThree = <button>You Shall Not Pass</button>;
+    return (
+      <div>
+        <h3>Enter Your Age to Continue</h3>
+        <input
+          style={inputStyle}
+          type="number"
+          value={this.state.input}
+          onChange={this.handleChange} /><br />
+        {
+          /* change code here */
+        }
+      </div>
+    );
+  }
+};
 
 /* Challenge 43 */
 /*  */
@@ -1102,4 +1170,16 @@ class MyComponent extends React.Component {
 
 
 /* Challenge 45 */
+/*  */
+
+
+/* Challenge 46 */
+/*  */
+
+
+/* Challenge 47 */
+/*  */
+
+
+/* Challenge 48 */
 /*  */
