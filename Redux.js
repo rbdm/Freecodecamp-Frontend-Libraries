42d35ef0381e8d13cb08248c65567b5370caf42a
ixport <mirhadydorodjatun@gmail.com>
2018-09-19 20:48:20 +0700

    Add solutions to Redux Chapter up to Challenge #13

--- Redux.js
+++ Redux.js
@@ -314,15 +314,45 @@ const store = Redux.createStore(
 );
 
 /* Challenge 13 */
-/*  */
+/* Write a Counter with Redux */
+const INCREMENT = 'INCREMENT'; // define a constant for increment action types
+const DECREMENT = 'DECREMENT'; // define a constant for decrement action types
 
+const defaultState = 0;
+
+const counterReducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+      break;
+    case DECREMENT:
+      return state - 1;
+      break;
+    default:
+      return state;
+  }
+}; // define the counter reducer which will increment or decrement the state based on the action it receives
+
+const incAction = () => {
+  return {
+    type: INCREMENT
+  }
+}; // define an action creator for incrementing
+
+const decAction = () => {
+  return {
+    type: DECREMENT
+  } // define an action creator for decrementing
+};
+
+const store = Redux.createStore(counterReducer); // define the Redux store here, passing in your reducers
 
 /* Challenge 14 */
-/*  */
+/* Never Mutate State */
 
 
 /* Challenge 15 */
-/*  */
+/* Use the Spread Operator on Arrays */
 
 
 /* Challenge 16 */
