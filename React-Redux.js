ca5a575e259111675015fea3eb54a60a86bca3e5
rbdm <mirhadydorodjatun@gmail.com>
2018-09-27 10:49:35 +0700

    solutions to React-Redux Chapter up to challenge 10

--- React-Redux.js
+++ React-Redux.js
@@ -385,28 +385,103 @@ class AppWrapper extends React.Component {
 };
 
 /* Challenge 10 */
-/*  */
+/* Extract Local State into Redux */
+// Redux:
+const ADD = 'ADD';
 
+const addMessage = (message) => {
+  return {
+    type: ADD,
+    message: message
+  }
+};
 
-/* Challenge 12 */
-/*  */
+const messageReducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD:
+      return [
+        ...state,
+        action.message
+      ];
+    default:
+      return state;
+  }
+};
 
+const store = Redux.createStore(messageReducer);
 
-/* Challenge 13 */
-/*  */
+// React:
+const Provider = ReactRedux.Provider;
+const connect = ReactRedux.connect;
 
+// Change code below this line
+class Presentational extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      //messages: [] <--moved to Redux
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.submitMessage = this.submitMessage.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+  submitMessage() {
+    this.props.submitNewMessage(this.state.input);
+    this.setState({
+      input: '',
+      //messages: this.state.messages.concat(this.state.input) <--modified to dispatch submitNewMessage from this.props
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h2>Type in a new Message:</h2>
+        <input
+          value={this.state.input}
+          onChange={this.handleChange}/><br/>
+        <button onClick={this.submitMessage}>Submit</button>
+        <ul>
+          {this.props.messages.map( (message, idx) => {
+              return (
+                 <li key={idx}>{message}</li>
+              )
+            })
+          }
+        </ul>
+      </div>
+    );
+  }
+};
+// Change code above this line
 
-/* Challenge 14 */
-/*  */
+const mapStateToProps = (state) => {
+  return {messages: state}
+};
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    submitNewMessage: (message) => {
+      dispatch(addMessage(message))
+    }
+  }
+};
 
-/* Challenge 15 */
-/*  */
+const Container = connect(mapStateToProps, mapDispatchToProps)(Presentational);
 
+class AppWrapper extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Container/>
+      </Provider>
+    );
+  }
+};
 
-/* Challenge 16 */
-/*  */
-
-
-/* Challenge 1 */
-/*  */
+/* Challenge 11 */
+/* Moving Forward From Here */
